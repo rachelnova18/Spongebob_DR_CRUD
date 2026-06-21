70de1bd2efc6a956384c6a8989b7f2dfc9acc124
rachelnova18 <rachelnv26@gmail.com>
2026-06-21 23:36:33 +0700

    report

---
+++ CRUDMahasiswaADO/Report.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRUDMahasiswaADO
+{
+    public partial class Report : Form
+    {
+        static string connectionString = @"Data Source=.\ACHELL;Initial Catalog=DBAkademikADO;User ID=sa;Password=Rachel18*";
+
+        SqlConnection conn = new SqlConnection(connectionString);
+        SqlDataAdapter da;
+        DataTable dtMahasiswa;
+        DataTable dtProdi;
+        DAL dbLogic = new DAL();
+
+        string prodi { get; set; }
+
+        DateTime tglmasuk { get; set; }
+
+        public Report(string Prodi, DateTime TglMasuk)
+        {
+            InitializeComponent();
+
+            prodi = Prodi;
+            tglmasuk = TglMasuk;
+
+            try
+            {
+                DataTable dtMahasiswa = dbLogic.getDataRekap(prodi, tglmasuk);
+
+                // Pastikan baris ini diaktifkan kembali jika sebelumnya Anda comment (//)
+                CrystalReport1 listMahasiswa = new CrystalReport1();
+                listMahasiswa.SetDataSource(dtMahasiswa);
+
+                crystalReportViewer1.ReportSource = listMahasiswa;
+                crystalReportViewer1.Refresh();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Gagal load data: " + ex.Message);
+            }
+        }
+
+        private void crystalReportViewer1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+     
+    }
+}
