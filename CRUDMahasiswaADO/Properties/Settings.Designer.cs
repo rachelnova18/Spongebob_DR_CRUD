582da7d358c4c284f196573dd5eae6973e1112ad
rachelnova18 <rachelnv26@gmail.com>
2026-06-21 23:35:33 +0700

    properti

--- CRUDMahasiswaADO/Properties/Settings.Designer.cs
+++ CRUDMahasiswaADO/Properties/Settings.Designer.cs
@@ -8,22 +8,40 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace CRUDMahasiswaADO.Properties
-{
-
-
+namespace CRUDMahasiswaADO.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.14.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-R4G2LB6\\ACHELL;Initial Catalog=DBAkademikADO;User ID=sa;Passw" +
+            "ord=Rachel18;TrustServerCertificate=True")]
+        public string DBAkademikADOConnectionString {
+            get {
+                return ((string)(this["DBAkademikADOConnectionString"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=Achel\\ACHELL;Initial Catalog=DBAkademikADO;Persist Security Info=True" +
+            ";User ID=sa;Password=Rachel18*;TrustServerCertificate=True")]
+        public string DBAkademikADOConnectionString1 {
+            get {
+                return ((string)(this["DBAkademikADOConnectionString1"]));
             }
         }
     }
